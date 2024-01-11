1fd8fa178cf665cadbac2abd20d8090d38ec9ad4
Programmer.md.sifat <100550955+mshsifat@users.noreply.github.com>
2024-01-11 00:09:09 -0800

    Update VarLetConst.js

--- VarLetConst.js
+++ VarLetConst.js
@@ -4,7 +4,7 @@ var a = 10;
 var b = 13;
 var c = a + b;
 
-console.log(c);
+console.log(c); // Output: 23
 
 
 // let code ex:
@@ -12,13 +12,13 @@ console.log(c);
 let age = 20;
 age = 25;
 
-console.log(age);
+console.log(age); // Output: 25
 
 // const code ex:
 
 const sifat = 50;
 
-console.log(sifat);
+console.log(sifat); // Output: 50
 
 
 // My Journey Share With Github
