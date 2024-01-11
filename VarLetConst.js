c52eb9ea5ebb440811607fe1c46e05630818834b
Programmer.md.sifat <100550955+mshsifat@users.noreply.github.com>
2024-01-10 23:51:39 -0800

    VarLetConst.js
    My Journey Share With Github.

---
+++ VarLetConst.js
@@ -0,0 +1,25 @@
+// var code ex:
+
+var a = 10;
+var b = 13;
+var c = a + b;
+
+console.log(c);
+
+
+// let code ex:
+
+let age = 20;
+age = 25;
+
+console.log(age);
+
+// const code ex:
+
+const sifat = 50;
+
+console.log(sifat);
+
+
+// My Journey Share With Github
+
