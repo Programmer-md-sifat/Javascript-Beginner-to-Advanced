b6d2ffd65dd0380dbb15941ebd343e3f151e0463
Md. Shahadat Hasan Sifat <100550955+Programmer-md-sifat@users.noreply.github.com>
2024-04-25 04:26:24 +0600

    Add files via upload

---
+++ Javascript-beginner to advance/Function&WithoutFunction/ex1.js
@@ -0,0 +1,26 @@
+// Function & Without Function Coding Ex:
+
+
+// Function:
+
+function addNumbers(num1, num2) {
+
+    return num1 + num2;
+  }
+  
+const result = addNumbers(3, 7);
+
+console.log("The sum is:", result);
+
+  
+// Without Function:
+
+const num1 = 3;
+const num2 = 7;
+
+const sum = num1 + num2;
+
+console.log("The sum is:", sum);
+
+
+// My Journey Share With Github
